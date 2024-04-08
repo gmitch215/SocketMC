e121b50d3ea3a36c6579df5b1c4e0671993d568c
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-04-07 20:38:14 -0500

    Remove Mod Listeners, Add Loggers

--- mod/fabric/src/main/java/me/gamercoder215/socketmc/fabric/FabricSocketMC.java
+++ mod/fabric/src/main/java/me/gamercoder215/socketmc/fabric/FabricSocketMC.java
@@ -1,17 +1,31 @@
 package me.gamercoder215.socketmc.fabric;
 
-import me.gamercoder215.socketmc.SocketMC;
-import me.gamercoder215.socketmc.events.ModListener;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
+import java.util.logging.Logger;
+
 @Environment(EnvType.CLIENT)
 public final class FabricSocketMC implements ClientModInitializer {
 
+    public static final Logger LOGGER = Logger.getLogger("SocketMC");
+
     @Override
     public void onInitializeClient() {
-        SocketMC.getListeners().forEach(ModListener::onInitialize);
+
+    }
+
+    public static void print(Throwable t) {
+        LOGGER.severe(t.getClass().getSimpleName());
+        LOGGER.severe("-----------");
+        LOGGER.severe(t.getMessage());
+        for (StackTraceElement element : t.getStackTrace()) LOGGER.severe(element.toString());
+
+        if (t.getCause() != null) {
+            LOGGER.severe("Caused by:");
+            print(t.getCause());
+        }
     }
 
 }
--- mod/forge/src/main/java/me/gamercoder215/socketmc/forge/ForgeSocketMC.java
+++ mod/forge/src/main/java/me/gamercoder215/socketmc/forge/ForgeSocketMC.java
@@ -1,14 +1,29 @@
 package me.gamercoder215.socketmc.forge;
 
 import me.gamercoder215.socketmc.SocketMC;
-import me.gamercoder215.socketmc.events.ModListener;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.logging.Logger;
 
 @Mod(SocketMC.MOD_ID)
 public final class ForgeSocketMC {
 
+    public static final Logger LOGGER = Logger.getLogger("SocketMC");
+
     public ForgeSocketMC() {
-        SocketMC.getListeners().forEach(ModListener::onInitialize);
+
+    }
+
+    public static void print(Throwable t) {
+        LOGGER.severe(t.getClass().getSimpleName());
+        LOGGER.severe("-----------");
+        LOGGER.severe(t.getMessage());
+        for (StackTraceElement element : t.getStackTrace()) LOGGER.severe(element.toString());
+
+        if (t.getCause() != null) {
+            LOGGER.severe("Caused by:");
+            print(t.getCause());
+        }
     }
 
 }
