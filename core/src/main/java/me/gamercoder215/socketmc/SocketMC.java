d41923736367c73032b52cf712dd049745db64df
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-04-14 16:04:18 -0500

    Move Logging to Core Class

--- core/src/main/java/me/gamercoder215/socketmc/SocketMC.java
+++ core/src/main/java/me/gamercoder215/socketmc/SocketMC.java
@@ -1,5 +1,7 @@
 package me.gamercoder215.socketmc;
 
+import java.util.logging.Logger;
+
 /**
  * Main utility class for managing the SocketMC API.
  */
@@ -9,5 +11,22 @@ public final class SocketMC {
      * The Mod ID for SocketMC.
      */
     public static final String MOD_ID = "socketmc";
+
+    /**
+     * Prints a stack trace to the logger.
+     * @param logger The logger to print to
+     * @param t The throwable to print
+     */
+    public static void print(Logger logger, Throwable t) {
+        logger.severe(t.getClass().getSimpleName());
+        logger.severe("-----------");
+        logger.severe(t.getMessage());
+        for (StackTraceElement element : t.getStackTrace()) logger.severe(element.toString());
+
+        if (t.getCause() != null) {
+            logger.severe("Caused by:");
+            print(logger, t.getCause());
+        }
+    }
 
 }
