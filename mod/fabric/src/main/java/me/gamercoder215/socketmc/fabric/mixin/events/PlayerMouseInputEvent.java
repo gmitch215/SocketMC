ce72dd892f723e3579120b277a09e6dc8256917f
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-04-27 23:56:07 -0500

    Create Pause in AsyncPlayerMoveMouseEvent

--- mod/fabric/src/main/java/me/gamercoder215/socketmc/fabric/mixin/events/PlayerMouseInputEvent.java
+++ mod/fabric/src/main/java/me/gamercoder215/socketmc/fabric/mixin/events/PlayerMouseInputEvent.java
@@ -2,6 +2,7 @@ package me.gamercoder215.socketmc.fabric.mixin.events;
 
 import me.gamercoder215.socketmc.fabric.FabricSocketMC;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,8 +12,18 @@ import java.util.Map;
 @Mixin(targets = "net.minecraft.client.MouseHandler")
 public class PlayerMouseInputEvent {
 
+    @Unique
+    private int previousX = -1;
+
+    @Unique
+    private int previousY = -1;
+
     @Inject(method = "onMove", at = @At("HEAD"))
     private void onMove(long window, double x, double y, CallbackInfo ci){
+        if (Math.abs(previousX - x) < 10 || Math.abs(previousY - y) < 10) return;
+
+        previousX = (int) x;
+        previousY = (int) y;
         FabricSocketMC.sendEvent(1, Map.of("x", x, "y", y));
     }
 
