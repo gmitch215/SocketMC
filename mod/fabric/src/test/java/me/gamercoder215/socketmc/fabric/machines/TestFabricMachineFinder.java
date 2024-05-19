e06b127fb72dc567342e71cc4360590b86003726
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-05-18 22:54:56 -0500

    Add `MACHINE` Field Test

--- mod/fabric/src/test/java/me/gamercoder215/socketmc/fabric/machines/TestFabricMachineFinder.java
+++ mod/fabric/src/test/java/me/gamercoder215/socketmc/fabric/machines/TestFabricMachineFinder.java
@@ -16,6 +16,7 @@ public class TestFabricMachineFinder {
         for (String id : Instruction.ids()) {
             Machine m = FabricMachineFinder.getMachine(id);
             Assertions.assertNotNull(m, "Machine for id '" + id + "' not found");
+            Assertions.assertDoesNotThrow(() -> m.getClass().getDeclaredField("MACHINE"));
         }
     }
 
--- mod/forge/src/test/java/me/gamercoder215/socketmc/forge/machines/TestForgeMachineFinder.java
+++ mod/forge/src/test/java/me/gamercoder215/socketmc/forge/machines/TestForgeMachineFinder.java
@@ -16,6 +16,7 @@ public class TestForgeMachineFinder {
         for (String id : Instruction.ids()) {
             Machine m = ForgeMachineFinder.getMachine(id);
             Assertions.assertNotNull(m, "Machine for id '" + id + "' not found");
+            Assertions.assertDoesNotThrow(() -> m.getClass().getDeclaredField("MACHINE"));
         }
     }
 
