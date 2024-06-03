423ed6f1484eb259f6466d3652baf898cfbdee0d
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-06-02 22:03:13 -0500

    Improve Machine Finder Tests

--- mod/fabric/src/test/java/me/gamercoder215/socketmc/fabric/machines/TestFabricMachineFinder.java
+++ mod/fabric/src/test/java/me/gamercoder215/socketmc/fabric/machines/TestFabricMachineFinder.java
@@ -16,7 +16,7 @@ public class TestFabricMachineFinder {
         for (String id : Instruction.ids()) {
             Machine m = FabricMachineFinder.getMachine(id);
             Assertions.assertNotNull(m, "Machine for id '" + id + "' not found");
-            Assertions.assertDoesNotThrow(() -> m.getClass().getDeclaredField("MACHINE"));
+            Assertions.assertDoesNotThrow(() -> m.getClass().getDeclaredField("MACHINE"), "Missing 'MACHINE' field in machine for id '" + id + "'");
         }
     }
 
--- mod/forge/src/test/java/me/gamercoder215/socketmc/forge/machines/TestForgeMachineFinder.java
+++ mod/forge/src/test/java/me/gamercoder215/socketmc/forge/machines/TestForgeMachineFinder.java
@@ -16,7 +16,7 @@ public class TestForgeMachineFinder {
         for (String id : Instruction.ids()) {
             Machine m = ForgeMachineFinder.getMachine(id);
             Assertions.assertNotNull(m, "Machine for id '" + id + "' not found");
-            Assertions.assertDoesNotThrow(() -> m.getClass().getDeclaredField("MACHINE"));
+            Assertions.assertDoesNotThrow(() -> m.getClass().getDeclaredField("MACHINE"), "Missing 'MACHINE' field in machine for id '" + id + "'");
         }
     }
 
