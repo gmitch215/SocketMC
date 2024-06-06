a772152f4ca9dbbf03b6ae5a4e59510d8f3fb72e
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-06-06 13:55:17 -0500

    Create MainScreen.java

---
+++ mod/fabric/src/main/java/me/gamercoder215/socketmc/fabric/mod/MainScreen.java
@@ -0,0 +1,12 @@
+package me.gamercoder215.socketmc.fabric.mod;
+
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+public final class MainScreen extends Screen {
+
+    protected MainScreen(Component title) {
+        super(title);
+    }
+
+}
